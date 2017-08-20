67a937fed0865f8f8f3046b9cf89f142cdc96363
JinJinRu <jin.jinru840430@gmail.com>
2017-08-20 04:41:12 -0400

    Fixed following log file issue Backup log file when size is exceeded or rvcd is starting

--- src/rvcd/src/log.c
+++ src/rvcd/src/log.c
@@ -29,6 +29,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <syslog.h>
 
@@ -36,7 +38,42 @@
 #include "log.h"
 
 static FILE *g_log_fp;
+static int g_log_fsize;
 static pthread_mutex_t g_log_mt = PTHREAD_MUTEX_INITIALIZER;
+
+/*
+ * create log file
+ */
+
+static int create_log_file()
+{
+	int fd;
+
+	/* check whether file pointer is exist */
+	if (g_log_fp) {
+		fclose(g_log_fp);
+		g_log_fp = NULL;
+	}
+
+	/* backup old log file */
+	rename(RVCD_LOG_FPATH, RVCD_LOG_BACKUP_FPATH);
+
+	/* open log file */
+	fd = open(RVCD_LOG_FPATH, O_CREAT | O_WRONLY);
+	if (fd > 0)
+		g_log_fp = fdopen(fd, "w");
+
+	if (fd < 0 || !g_log_fp) {
+		fprintf(stderr, "Couldn't open log file '%s' for writing.\n", RVCD_LOG_FPATH);
+
+		if (fd > 0)
+			close(fd);
+
+		return -1;
+	}
+
+	return 0;
+}
 
 /*
  * initialize rvcd logging
@@ -45,11 +82,8 @@ static pthread_mutex_t g_log_mt = PTHREAD_MUTEX_INITIALIZER;
 int rvcd_log_init()
 {
 	/* open log file */
-	g_log_fp = fopen(RVCD_LOG_FPATH, "w");
-	if (!g_log_fp) {
-		fprintf(stderr, "Couldn't open log file '%s' for writing.\n", RVCD_LOG_FPATH);
+	if (create_log_file() != 0)
 		return -1;
-	}
 
 	/* open syslog */
 	openlog(NULL, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_USER);
@@ -94,6 +128,7 @@ void rvcd_debug_log(enum LOG_TYPE log_type, const char *file_name, int file_line
 	char time_str[64];
 
 	char msg[RVCD_MAX_LOGMSG_LEN + 1];
+	int written_log_bytes;
 
 	/* check log file pointer */
 	if (!g_log_fp)
@@ -101,6 +136,17 @@ void rvcd_debug_log(enum LOG_TYPE log_type, const char *file_name, int file_line
 
 	/* mutex lock */
 	pthread_mutex_lock(&g_log_mt);
+
+	/* check log file size */
+	if (!g_log_fp || g_log_fsize > RVCD_MAX_LOG_FSIZE) {
+		create_log_file();
+		g_log_fsize = 0;
+	}
+
+	if (!g_log_fp) {
+		pthread_mutex_unlock(&g_log_mt);
+		return;
+	}
 
 	/* build log string */
 	va_start(va_args, format);
@@ -113,7 +159,10 @@ void rvcd_debug_log(enum LOG_TYPE log_type, const char *file_name, int file_line
 	strftime(time_str, sizeof(time_str), "%a %d %b %Y %T", tm);
 
 	/* write log */
-	fprintf(g_log_fp, "%s\t%s : %s(at %s:%d)\n", time_str, log_type_str[log_type], msg, file_name, file_line);
+	written_log_bytes = fprintf(g_log_fp, "%s\t%s : %s(at %s:%d)\n", time_str, log_type_str[log_type], msg, file_name, file_line);
+	if (written_log_bytes > 0)
+		g_log_fsize += written_log_bytes;
+
 	fflush(g_log_fp);
 
 	fprintf(stderr, "%s\t%s : %s(at %s:%d)\n", time_str, log_type_str[log_type], msg, file_name, file_line);
