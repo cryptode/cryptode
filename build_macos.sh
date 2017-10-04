ccdcf07f0223d55a76f09adbdf79a54752effe4d
Jin JinRu <jin.jinru840430@gmail.com>
2017-10-04 13:45:16 -0400

    fixed some typo

--- build_macos.sh
+++ build_macos.sh
@@ -3,7 +3,7 @@
 # check whether openssl was installed on mac
 OPENSSL_DIR=$(brew --prefix openssl)
 if [ -z "$OPENSSL_DIR" ]; then
-	echo "Couldn't find openssl installation directory. Try to 'brew install openssl'"
+	echo "Couldn't find openssl installation directory. Try to run 'brew install openssl'"
 	exit 1
 fi
 
