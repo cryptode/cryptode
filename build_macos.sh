88973ad5ec643ff206864fcc034696a4e18ca643
Jin JinRu <jin.jinru840430@gmail.com>
2017-10-04 13:44:01 -0400

    Added script to build rvc on mac

---
+++ build_macos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# check whether openssl was installed on mac
+OPENSSL_DIR=$(brew --prefix openssl)
+if [ -z "$OPENSSL_DIR" ]; then
+	echo "Couldn't find openssl installation directory. Try to 'brew install openssl'"
+	exit 1
+fi
+
+# try to compile rvc on mac
+./autogen.sh
+./configure --with-openssl=$OPENSSL_DIR
+make
